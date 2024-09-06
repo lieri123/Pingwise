9167a108f59ee350cc1d5252f83dfcd5e8eaa5de
lieri123 <142183090+lieri123@users.noreply.github.com>
2024-09-06 19:31:13 -0400

    Identify the unpaidMembers in the excel spreadsheet

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ sheet = wb.get_sheet_by_name("Sheet1")
 lastCol = sheet.max_column
 latestMonth = sheet.cell(row=1, column=lastCol).value
 
+unpaidMembers = {}
+for r in range(2, sheet.max_row + 1):
+    payment = sheet.cell(row=r, column=lastCol).value
+    if payment != 'paid':
+        name = sheet.cell(row= r, column=1).value
+        email = sheet.cell(row = r, column=2).value 
+
+        unpaidMembers[name] = email
+
+
